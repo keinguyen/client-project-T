cb50c7f7667f1d42fcbd02eafc85c84164e8bbde
Huu Le <huule@ADMIN-2.local>
2024-02-05 23:59:11 +0700

    update server api

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,12 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { socketService } from '../services/socketService';
 
 export function Main() {
+  const [accept, setAccept] = useState(false);
+  const [dolbyData, setDolbyData] = useState<{
+    streamName: string;
+    accountId: string;
+  }>();
+
   useEffect(() => {
-    socketService.subscribeToJoinConversation(({ accountId, streamName }) => {
-      console.log('****** accountId ******', accountId);
-      console.log('****** streamName ******', streamName);
+    socketService.subscribeToJoinConversation((data) => {
+      setDolbyData(data);
     });
   }, []);
-  return <>Hello</>;
+
+  return (
+    <>
+      <>
+        {dolbyData?.streamName ? (
+          <>
+            <div>Bạn có yêu cầu từ đại lý</div>
+            <button onClick={() => setAccept(true)}>Accept</button>
+          </>
+        ) : (
+          <div>Xin chào bạn đến hệ thống kiểm định</div>
+        )}
+
+        {dolbyData?.streamName && accept && (
+          <iframe
+            src={`https://viewer.millicast.com?streamId=${dolbyData.accountId}/${dolbyData.streamName}`}
+            allowFullScreen
+            width="640"
+            height="480"
+          ></iframe>
+        )}
+      </>
+    </>
+  );
 }
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -3,7 +3,7 @@ import { ClientToServerEvents, ServerToClientEvents } from '../models/event';
 
 class SocketService {
   private readonly socket: Socket<ServerToClientEvents, ClientToServerEvents> =
-    io('ws://appraisal-hub.onrender.com', {
+    io('wss://appraisal-hub.onrender.com', {
       autoConnect: true,
       transports: ['websocket'],
     });
